989caf115ad6664fa5f6745223e00906b4db1349
Graeme Brown <graemehbrown@googlemail.com>
2017-12-16 14:29:35 +0000

    Added update() instance method to Eatery class.  Also started to create some basic tests for Eatery methods in console.  These are linkely to be moved separate spec file.

--- console.rb
+++ console.rb
@@ -4,7 +4,6 @@ require_relative('./models/eatery.rb')
 bobs_burgers = Eatery.new({'name' => 'Bobs Burgers', 'location' => 'Edinburgh'})
 bobs_burgers.save()
 
-
 burgers_r_us = Eatery.new({'name' => 'Burgers R Us', 'location' => 'Edinburgh'})
 burgers_r_us.save()
 
@@ -12,10 +11,29 @@ all_eateries = Eatery.all()
 
 found_eatery = Eatery.find(bobs_burgers.id)
 
+###test for deletion of instance
 bobs_burgers.delete()
+begin
+   Eatery.find(bobs_burgers.id)
+rescue => e
+  if e.class.name == 'NoMethodError'
+    p"----------Delete successful can't call find on nil object."
+  else
+    p "---------Delete failed."
+  end
+end
 
-found_eatery = Eatery.find(bobs_burgers.id)
+###test for update
+new_name = 'Burger meats Burger'
+burgers_r_us.name = new_name
+burgers_r_us.update()
+updated_eatery = Eatery.find(burgers_r_us.id)
+if updated_eatery.name == new_name
+  p "---------Name updated successfully."
+else
+  p "---------Updated failed!"
+end
+
 
 binding.pry
-
 nil
--- models/eatery.rb
+++ models/eatery.rb
@@ -27,27 +27,42 @@ class Eatery
     @id = results.first()['id'].to_i
   end
 
-  def delete()
-    sql = "DELETE FROM eateries
-    WHERE id = $1"
-    values = [@id]
-    SqlRunner.run(sql, values)
+  def update()
+    sql = "UPDATE eateries
+    SET
+    (
+      name,
+      location
+      ) =
+      (
+        $1, $2
+      )
+      WHERE id = $3"
+      values = [@name, @location, @id]
+      SqlRunner.run(sql, values)
+    end
+
+    def delete()
+      sql = "DELETE FROM eateries
+      WHERE id = $1"
+      values = [@id]
+      SqlRunner.run(sql, values)
+    end
+
+    def self.all()
+      sql = "SELECT * FROM eateries;"
+      values = []
+      results = SqlRunner.run( sql, values )
+      return results.map { |eatery| Eatery.new( eatery ) }
+    end
+
+    def self.find(id)
+      sql = "SELECT * FROM eateries
+      WHERE id = $1"
+      values = [id]
+      results = SqlRunner.run(sql, values)
+      return Eatery.new(results.first)
+    end
+
+
   end
-
-  def self.all()
-    sql = "SELECT * FROM eateries;"
-    values = []
-    results = SqlRunner.run( sql, values )
-    return results.map { |eatery| Eatery.new( eatery ) }
-  end
-
-  def self.find(id)
-    sql = "SELECT * FROM eateries
-    WHERE id = $1"
-    values = [id]
-    results = SqlRunner.run(sql, values)
-    return Eatery.new(results.first)
-  end
-
-
-end
