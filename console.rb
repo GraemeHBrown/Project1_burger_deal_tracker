dc6f42d7aae94ac2173deaff6606e86d6bafb4ba
Graeme Brown <graemehbrown@googlemail.com>
2017-12-16 13:44:56 +0000

    Added delete() instance method to delete one eatery.  Also added self.all() and self.find(id) class methods to find all eateries and a single instance by id.

--- console.rb
+++ console.rb
@@ -4,6 +4,18 @@ require_relative('./models/eatery.rb')
 bobs_burgers = Eatery.new({'name' => 'Bobs Burgers', 'location' => 'Edinburgh'})
 bobs_burgers.save()
 
+
+burgers_r_us = Eatery.new({'name' => 'Burgers R Us', 'location' => 'Edinburgh'})
+burgers_r_us.save()
+
+all_eateries = Eatery.all()
+
+found_eatery = Eatery.find(bobs_burgers.id)
+
+bobs_burgers.delete()
+
+found_eatery = Eatery.find(bobs_burgers.id)
+
 binding.pry
 
 nil
--- models/eatery.rb
+++ models/eatery.rb
@@ -2,8 +2,8 @@ require_relative('../db/sql_runner.rb')
 
 class Eatery
 
-attr_reader :id
-attr_accessor :name, :location
+  attr_reader :id
+  attr_accessor :name, :location
 
   def initialize( options )
     @id = options['id'].to_i if options['id']
@@ -12,19 +12,42 @@ attr_accessor :name, :location
   end
 
   def save()
-  sql = "INSERT INTO eateries
-  (
-    name,
-    location
-  )
-  VALUES
-  (
-    $1, $2
-  )
-  RETURNING id"
-  values = [@name, @location]
-  results = SqlRunner.run(sql, values)
-  @id = results.first()['id'].to_i
-end
+    sql = "INSERT INTO eateries
+    (
+      name,
+      location
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@name, @location]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def delete()
+    sql = "DELETE FROM eateries
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM eateries;"
+    values = []
+    results = SqlRunner.run( sql, values )
+    return results.map { |eatery| Eatery.new( eatery ) }
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM eateries
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Eatery.new(results.first)
+  end
+
 
 end
