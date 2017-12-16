d5409124b08de48a00c43f6631a867d29f292d72
Graeme Brown <graemehbrown@googlemail.com>
2017-12-16 17:17:17 +0000

    Added delete action method to eateries_controller and new action method.  Also added show.erd to diplay details of one eatery.

--- controllers/eateries_controller.rb
+++ controllers/eateries_controller.rb
@@ -9,9 +9,22 @@ get('/eateries') do
   erb :index
 end
 
-#show diplays one eatery in view based on id in URL
+#show action diplays one eatery in view based on id in URL
 get('/eateries/:id') do
   id = params[:id]
   @eatery = Eatery.find(id)
   erb :show
+end
+
+#delete action deletes one eatery based on ID in URL
+post('/eateries/:id/delete') do
+  id = params[:id]
+  @eatery = Eatery.find(id)
+  @eatery.delete()
+  redirect'/eateries'
+end
+
+#new action
+get('/eateries/new') do
+  erb :new
 end
