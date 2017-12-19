a0fa64b9aaf344575db04ab33229d4f14205f633
Graeme Brown <graemehbrown@googlemail.com>
2017-12-19 12:49:52 +0000

    Added edit and update actions to deals_controller.rb.  Also added deals/edit.erb and deals/show.erb views for edit and update actions.

--- controllers/deals_controller.rb
+++ controllers/deals_controller.rb
@@ -15,3 +15,35 @@ get('/deals') do
   end
   erb (:"deals/index")
 end
+
+#show action diplays one deal in view based on id in URL
+get('/deals/:id') do
+  id = params[:id]
+  @deal = Deal.find(id)
+  erb (:"deals/show")
+end
+
+#edit action
+get('/deals/:id/edit') do
+  id = params[:id]
+  @deal = Deal.find(id)
+  erb (:"deals/edit")
+end
+
+#update action
+post('/deals/:id') do
+  id = params[:id]
+  deal = Deal.find(id)
+  params[:eatery_id] = deal.eatery_id
+  @updated_deal = Deal.new(params)
+  @updated_deal.update()
+  redirect '/deals'
+end
+
+#delete action deletes one deal based on ID in URL
+post('/deals/:id/delete') do
+  id = params[:id]
+  @deal = Deal.find(id)
+  @deal.delete()
+  redirect'/deals'
+end
