42eb4491d81de0196a1c1cb6d9c154b9aceed3c6
Graeme Brown <graemehbrown@googlemail.com>
2017-12-20 08:51:31 +0000

    Added public folder with images and styles sub-folders.  Added first css file for message styles.
    Added link to main layout.erb to access searches.

--- db/queries.sql
+++ db/queries.sql
@@ -1,3 +1,6 @@
+------- Query scratchpad -----------
+
+
 -- SELECT * FROM burgers WHERE eatery_id = 3;
 
 -- SELECT deals.* FROM deals INNER JOIN burgers ON burgers.id = deals.burger_id WHERE burgers.eatery_id = 259;
